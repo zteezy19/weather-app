3a3eee6285e76ac09d74247b73b32b7bdc548620
zteezy19 <waiyanzawtin01@protonmail.com>
2024-05-10 17:40:22 +0800

    Added 2 new Weather components

---
+++ src/components/WeatherDetails.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { LuEye, LuSunrise, LuSunset } from "react-icons/lu";
+import { FiDroplet } from "react-icons/fi";
+import { MdAir } from "react-icons/md";
+import { ImMeter } from "react-icons/im";
+
+export interface WeatherDetailProps {
+  visibility: string;
+  humidity: string;
+  windSpeed: string;
+  airPressure: string;
+  sunrise: string;
+  sunset: string;
+}
+
+export interface SingleWeatherDetailProps {
+  information: string;
+  icon: React.ReactNode;
+  value: string;
+}
+
+export default function WeatherDetails(props: WeatherDetailProps) {
+  const {
+    visibility = "420km",
+    humidity = "100%",
+    windSpeed = "69 km/h",
+    airPressure = "1007 hPa",
+    sunrise = "6.20",
+    sunset = "18:48",
+  } = props;
+
+  return (
+    <>
+      <SingleWeatherDetail
+        icon={<LuEye />}
+        information="Visability"
+        value={visibility}
+      />
+      <SingleWeatherDetail
+        icon={<FiDroplet />}
+        information="Humidity"
+        value={humidity}
+      />
+      <SingleWeatherDetail
+        icon={<MdAir />}
+        information="Wind speed"
+        value={windSpeed}
+      />
+      <SingleWeatherDetail
+        icon={<ImMeter />}
+        information="Air Pressure"
+        value={airPressure}
+      />
+      <SingleWeatherDetail
+        icon={<LuSunrise />}
+        information="Sunrise"
+        value={sunrise}
+      />
+      <SingleWeatherDetail
+        icon={<LuSunset />}
+        information="Sunset"
+        value={sunset}
+      />
+    </>
+  );
+}
+
+function SingleWeatherDetail(props: SingleWeatherDetailProps) {
+  return (
+    <div className="flex flex-col justify-between gap-2 items-center text-xs font-semibold text-black/80">
+      <p className="whitespace-nowrap">{props.information}</p>
+      <div className="text-3xl">{props.icon}</div>
+      <p>{props.value}</p>
+    </div>
+  );
+}
---
+++ src/utils/convertKelvinToCelsius.ts
@@ -0,0 +1,4 @@
+export function convertKelvintoCelsius(tempInKelvin: number): number {
+  const tempInCelsius = tempInKelvin - 273.15;
+  return Math.floor(tempInCelsius);
+}
